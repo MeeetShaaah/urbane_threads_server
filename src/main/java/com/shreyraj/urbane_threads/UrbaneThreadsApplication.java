9dd4e15bb3fd1dd38bd456ce132cc9918f09411e
MeeetShaaah <meetshah7199@gmail.com>
2024-01-31 15:44:26 -0500

    Start with urbane_threads server.

---
+++ src/main/java/com/shreyraj/urbane_threads/UrbaneThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.shreyraj.urbane_threads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbaneThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbaneThreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyraj/urbane_threads/UrbaneThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyraj.urbane_threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbaneThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
