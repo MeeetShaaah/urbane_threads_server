328e882fa4ccf14819d9338bed9ec95df8d49ea5
MeeetShaaah <meetshah7199@gmail.com>
2024-01-31 17:40:51 -0500

    Create Entity Class.

---
+++ src/main/java/com/shreyraj/urbane_threads/model/PaymentInformation.java
@@ -0,0 +1,25 @@
+package com.shreyraj.urbane_threads.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+
+public class PaymentInformation { 
+
+	    @Column(name = "cardholder_name")
+	    private String cardholderName;
+
+	    @Column(name = "card_number")
+	    private String cardNumber;
+
+	    @Column(name = "expiration_date")
+	    private LocalDate expirationDate;
+
+	    @Column(name = "cvv")
+	    private String cvv;
+
+	    // getters and setters
+	}
+
+
+
