2b0347d3d7ebc8bd3c61ad2143d684e2142e4aa7
MeeetShaaah <meetshah7199@gmail.com>
2024-01-31 17:01:47 -0500

    NA

--- src/main/java/com/shreyraj/urbane_threads/config/JwtTokenProvider.java
+++ src/main/java/com/shreyraj/urbane_threads/config/JwtTokenProvider.java
@@ -1,15 +1,10 @@
 package com.shreyraj.urbane_threads.config;
 
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.crypto.SecretKey;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
