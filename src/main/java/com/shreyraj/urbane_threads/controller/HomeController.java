55f0aa94ab2f3c4cb75b57e7b740c5214eede7d5
MeeetShaaah <meetshah7199@gmail.com>
2024-02-05 17:07:28 -0500

    Update Controller and service Class.

---
+++ src/main/java/com/shreyraj/urbane_threads/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.shreyraj.urbane_threads.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shreyraj.urbane_threads.response.ApiResponse;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public ResponseEntity<ApiResponse> homeController(){
+
+		ApiResponse res=new ApiResponse("Welcome To E-Commerce System", true);
+
+		return new ResponseEntity<>(res,HttpStatus.OK);
+	}
+}
