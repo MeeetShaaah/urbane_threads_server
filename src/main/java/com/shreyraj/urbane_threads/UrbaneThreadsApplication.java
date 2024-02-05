173280a37105ac366eee87a82bf7d1f06f58a918
MeeetShaaah <meetshah7199@gmail.com>
2024-02-05 17:17:17 -0500

    Add Swagger to application.

--- src/main/java/com/shreyraj/urbane_threads/UrbaneThreadsApplication.java
+++ src/main/java/com/shreyraj/urbane_threads/UrbaneThreadsApplication.java
@@ -3,7 +3,24 @@ package com.shreyraj.urbane_threads;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title="Ecommerce Project Api With Springboot",
+		description="Welcome to the documentation for the eCommerce Project API! This API provides a set of endpoints and functionalities to interact with our eCommerce platform programmatically. With this API, you can build applications, integrations, and automate various tasks related to our online store.",
+		version="v1",
+		contact=@Contact(
+				name="Code With ShreyRaj",
+				email="meetshah7199@gmail.com"
+				),
+		license=@License(
+				name="© Code With ShreyRaj"
+				)
+		))
 public class UrbaneThreadsApplication {
 
 	public static void main(String[] args) {
