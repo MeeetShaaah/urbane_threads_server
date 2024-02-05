73d4161fdb25f04f6bf98cf69a58afb3cf769759
MeeetShaaah <meetshah7199@gmail.com>
2024-02-05 17:03:33 -0500

    Update Token Class.

--- src/main/java/com/shreyraj/urbane_threads/config/JwtConstant.java
+++ src/main/java/com/shreyraj/urbane_threads/config/JwtConstant.java
@@ -1,7 +1,8 @@
 package com.shreyraj.urbane_threads.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY="meeku7109@@";
-	public static final String JWT_HEADER="Authorization";
+
+    public static final String SECRET_KEY="wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
+    public static final String JWT_HEADER="Authorization";
 
 }
